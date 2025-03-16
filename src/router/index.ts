77c861c5ddc56dfe193af134f4004a8f5db516af
Alexandre Fernandes <alexandrefer1231@gmail.com>
2025-03-15 23:09:51 -0300

    fix: vercel route problem

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import Login from '../views/Login.vue'
 import SignUp from '../views/SignUp.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
@@ -16,12 +16,8 @@ const router = createRouter({
       component: SignUp,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/:pathMatch(.*)*',
+      redirect: '/',
     },
   ],
 })
